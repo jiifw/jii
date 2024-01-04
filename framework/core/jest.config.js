63acfb609793f1c480da55c06d09701667fa780c
Junaid Atari <mj.atari@gmail.com>
2024-01-04 18:32:44 +0500

    add copyright

--- framework/core/jest.config.js
+++ framework/core/jest.config.js
@@ -1,3 +1,11 @@
+/**
+ * @link https://www.github.com/jiifw
+ * @copyright Copyright (c) 2023-2024 Jii framework
+ * @license https://www.github.com/jiifw/jii/blob/main/LICENSE
+ * @author Junaid Atari <mj.atari@gmail.com>
+ * @since 0.0.1
+ */
+
 const merge = require('deepmerge');
 
 // shared configs
