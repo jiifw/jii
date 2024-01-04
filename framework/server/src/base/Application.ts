0150ccd420d167acf244fa09161f310c90e514c7
Junaid Atari <mj.atari@gmail.com>
2024-01-04 19:40:31 +0500

    cleanup, extend with base class

--- framework/server/src/base/Application.ts
+++ framework/server/src/base/Application.ts
@@ -7,86 +7,28 @@
  */
 
 // classes
-import Component from '@jii/core/dist/classes/Component';
+import BaseApplication from '@jii/core/dist/classes/BaseApplication';
 
 // utils
 import Jii from '@jii/core/dist/Jii';
-import {bindClass} from '@jii/core/dist/helpers/auto-bind';
 
 // types
 import {ApplicationConfig} from '../typings/app';
-import {ServerInstance, ServerRequest, ServerReply} from '@jii/server/dist/typings/server';
-
-// scripts
-import configValidator from '../scripts/config-validator';
-
-export type Params = Record<string, any>;
 
 /**
  * Application class
  */
-export default abstract class Application<
-  S extends ServerInstance = ServerInstance,
-  Request extends ServerRequest = ServerRequest,
-  Reply extends ServerReply = ServerReply
-> extends Component {
-  private _id: string;
-  private _basePath: string;
-  private _params: Params;
-  protected _server: S;
-
+export default abstract class Application<Server, Request, Reply> extends BaseApplication<ApplicationConfig> {
   /**
-   * Application constructor
-   * @param config - Application configuration
+   * Server instance
+   * @protected
    */
-  constructor(
-    public readonly config: Partial<ApplicationConfig>,
-  ) {
-    super({});
-
-    bindClass(this);
-    this._initConfig();
-  }
-
-  /**
-   * Initialize application configuration
-   * @private
-   */
-  private _initConfig() {
-    // validates/verify configuration
-    configValidator(<ApplicationConfig>this.config);
-
-    this._id = this.config.id;
-    this._basePath = this.config.basePath;
-
-    // set params
-    this._params = this.config.params;
-
-    // register aliases
-    for (const key of Object.keys(this.config.aliases)) {
-      this.config.params[key] = this.config.aliases[key];
-      Jii.setAlias(key, this.config.aliases[key]);
-    }
-  }
-
-  /**
-   * Get application parameters
-   */
-  get params(): Params {
-    return this._params;
-  }
-
-  /**
-   * Get application base path
-   */
-  get basePath(): string {
-    return this._basePath;
-  }
+  protected _server: Server;
 
   /**
    * Get application server instance
    */
-  get server(): S {
+  get server(): Server {
     return this._server;
   }
 
