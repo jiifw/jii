5fa9862e3ae17aeb5828da6bd19d71183e601b50
Junaid Atari <mj.atari@gmail.com>
2024-01-04 19:35:36 +0500

    create files

---
+++ framework/core/src/classes/BaseApplication.ts
@@ -0,0 +1,95 @@
+/**
+ * @link https://www.github.com/jiifw
+ * @copyright Copyright (c) 2023-2024 Jii framework
+ * @license https://www.github.com/jiifw/jii/blob/main/LICENSE
+ * @author Junaid Atari <mj.atari@gmail.com>
+ * @since 0.0.1
+ */
+
+// classes
+import Component from './Component';
+
+// utils
+import Jii from '../Jii';
+import {CONTAINER_APP_KEY} from '../BaseJii';
+
+// scripts
+import configValidator from '../scripts/config-validator';
+
+// types
+import {ApplicationConfig} from '../typings/app-config';
+import {Props} from './BaseObject';
+
+// public types
+export type Params = Record<string, any>;
+
+/**
+ * Application class
+ */
+export default abstract class BaseApplication<
+  T extends ApplicationConfig = ApplicationConfig
+> extends Component {
+  /**
+   * Application unique identifier
+   * @private
+   */
+  private _id: string;
+
+  /**
+   * Application base path
+   * @private
+   */
+  private _basePath: string;
+
+  /**
+   * Application parameters
+   * @private
+   */
+  private _params: Params;
+
+  /**
+   * Application constructor
+   * @param config - Application configuration
+   * @param [props] - Component properties
+   */
+  constructor(public readonly config: T, props: Props = {}) {
+    super(props);
+    this._initConfig();
+    Jii.container.memoSync(CONTAINER_APP_KEY, this, {freeze: true});
+  }
+
+  /**
+   * Initialize application configuration
+   * @private
+   */
+  private _initConfig() {
+    // validates/verify configuration
+    configValidator(<ApplicationConfig>this.config);
+
+    this._id = this.config.id;
+    this._basePath = this.config.basePath;
+
+    // set params
+    this._params = this.config.params;
+
+    // register aliases
+    for (const key of Object.keys(this.config.aliases)) {
+      this.config.params[key] = this.config.aliases[key];
+      Jii.setAlias(key, this.config.aliases[key]);
+    }
+  }
+
+  /**
+   * Get application parameters
+   */
+  get params(): Params {
+    return this._params;
+  }
+
+  /**
+   * Get application base path
+   */
+  get basePath(): string {
+    return this._basePath;
+  }
+}
---
+++ framework/core/src/typings/app-config.ts
@@ -0,0 +1,70 @@
+/**
+ * @link https://www.github.com/jiifw
+ * @copyright Copyright (c) 2023-2024 Jii framework
+ * @license https://www.github.com/jiifw/jii/blob/main/LICENSE
+ * @author Junaid Atari <mj.atari@gmail.com>
+ * @since 0.0.1
+ */
+
+/**
+ * Application configuration typing interface
+ */
+export interface ApplicationConfig {
+  /**
+   * Application ID (e.g., 'ecommerce-platform' or 'superficial-project')
+   */
+  id: string;
+  /**
+   * Absolute path to the root directory of the application
+   */
+  basePath: string;
+  /**
+   * The absolute or alias path to the bootstrap file(s)<br>
+   * <span color="red">Warning</span>: Do not include extensions like '.ts' or '.js' in the path.<br>
+   * @example As a single file
+   * 'app/bootstrap/main'
+   * @example Multiple files
+   * ['@app/bootstrap/pre','app/bootstrap/post']
+   */
+  bootstrap?: string | Array<string>;
+  /**
+   * The absolute or alias directory path to the cli command files<br>
+   * @example As a single directory
+   * '@app/commands'
+   * @example Multiple directories
+   * ['@app/commands','app/some-dir/commands']
+   */
+  commands?: string | Array<string>;
+  /**
+   * Aliases for the application to register
+   * @example
+   * {
+   *   'somePath': '@app/bootstrap/pre',
+   *   '@path': path.dirname(__dirname),
+   *   ...
+   * }
+   */
+  aliases?: Record<string, string>;
+  /**
+   * Additional parameters to store with the application to access later<br>
+   * To access in later application, use: `Jii.instance().params['secret'];`
+   * @example
+   * {
+   *   'db': {server: 'localhost', ...},
+   *   'secret': '<random-hash-here>',
+   * }
+   */
+  params?: Record<string, any>;
+  /*modules?: {
+    [name: string]: {
+      [property: string]: any;
+    };
+  };*/
+  /*
+  components?: {
+    [name: string]: any;
+  };
+  middleware?: {
+    [name: string]: any;
+  };*/
+}
