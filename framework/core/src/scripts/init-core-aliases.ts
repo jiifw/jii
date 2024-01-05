7f2759d8c61de7637287255f7349310ae31d6ea5
Junaid Atari <mj.atari@gmail.com>
2024-01-05 12:46:27 +0500

    new script: init core aliases

---
+++ framework/core/src/scripts/init-core-aliases.ts
@@ -0,0 +1,22 @@
+/**
+ * @link https://www.github.com/jiifw
+ * @copyright Copyright (c) 2023-2024 Jii framework
+ * @license https://www.github.com/jiifw/jii/blob/main/LICENSE
+ * @author Junaid Atari <mj.atari@gmail.com>
+ * @since 0.0.1
+ */
+
+// utils
+import {dirname} from '../helpers/path';
+
+// classes
+import BaseJii from '../BaseJii';
+
+/**
+ * Initializes the core aliases
+ * @param jii - Jii instance
+ */
+export default (jii: InstanceType<typeof BaseJii>) => {
+  jii.setAlias('@jiiRoot', dirname(__filename, 5));
+  jii.setAlias('@jii', dirname(__filename, 2));
+}
