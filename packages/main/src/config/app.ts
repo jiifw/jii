9aa9d3e75c005e178b0d6b50c35906102c789fea
Junaid Atari <mj.atari@gmail.com>
2024-01-05 17:47:56 +0500

    fix typing file path

--- packages/main/src/config/app.ts
+++ packages/main/src/config/app.ts
@@ -1,8 +1,19 @@
+/**
+ * @link https://www.github.com/jiifw
+ * @copyright Copyright (c) 2023-2024 Jii framework
+ * @license https://www.github.com/jiifw/jii/blob/main/LICENSE
+ * @author Junaid Atari <mj.atari@gmail.com>
+ * @since 0.0.1
+ */
+
 import {dirname} from '@jii/core/dist/helpers/path';
 
 // types
-import {ApplicationConfig} from '@jii/server/dist/typings/app';
+import {ApplicationConfig} from '@jii/server/dist/typings/app-config';
 
+/**
+ * Application configuration.
+ */
 export default async (): Promise<ApplicationConfig> => ({
   id: 'jii-app',
   basePath: dirname(__dirname),
@@ -10,11 +21,10 @@ export default async (): Promise<ApplicationConfig> => ({
     test: 'value',
   },
   aliases: {
-    classed: 'x'
+    classed: 'x',
   },
   bootstrap: ['@app/bootstrap/bootstrap'],
-  server: {
-    httpOptions: {
-    }
-  }
+  /*server: {
+    httpOptions: {},
+  },*/
 });
