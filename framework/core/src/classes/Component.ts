e5b8acdaeb2c4463675d0c9299d6e29f0f1b8426
Junaid Atari <mj.atari@gmail.com>
2024-01-09 18:53:57 +0500

    enh: no need to check for undefined

--- framework/core/src/classes/Component.ts
+++ framework/core/src/classes/Component.ts
@@ -11,13 +11,16 @@ import BaseObject from './BaseObject';
 import Event from './Event';
 
 // utils
+import Jii from '../Jii';
 import {invoke, invokeMethod} from '../helpers/function';
 import {invokeModuleMethod} from '../helpers/file';
 
 // types
 import {EventHandler, EventData, Events} from './Event';
+import Behavior, {Instance as BehaviorInstance, BehaviorArgs} from './Behavior';
 
 // public types
+export type Instance = InstanceType<typeof Component>;
 export type {PropertyMeta, PropertyName, Props, PropertyScope} from './BaseObject';
 export type {EventHandler, EventData, Events} from './Event';
 
@@ -80,11 +83,16 @@ export default class Component extends BaseObject {
   private _events: Events = new Map;
 
   /**
+   * The attached behaviors (behavior name: behavior). This is `null` when not initialized.
+   */
+  private _behaviors: Map<string, BehaviorInstance> = new Map;
+
+  /**
    * Returns a value indicating whether there is any handler attached to the named event.
    * @param name - The event name
    * @returns Whether there is any handler attached to the event.
    */
-  public hasEventHandlers (name: string): boolean{
+  public hasEventHandlers(name: string): boolean {
     if (this._events.has(name)) {
       return true;
     }
@@ -116,7 +124,7 @@ export default class Component extends BaseObject {
    * @param name - The event name
    * @param handler - The event handler
    * @param data - The data to be passed to the event handler when the event is triggered.
-   * When the event handler is invoked, this data can be accessed via {@link Event.data Event.data}.
+   * When the event handler is invoked, this data can be accessed via {@link Event#data Event.data}.
    * @see {@link off off()}
    * @throws Error if the handler is not a valid callback
    */
@@ -163,7 +171,7 @@ export default class Component extends BaseObject {
       this._events.get(name).delete(handler);
 
       if (!this._events.get(name).size) {
-        this._events.delete(name)
+        this._events.delete(name);
       }
 
       return true;
@@ -200,7 +208,7 @@ export default class Component extends BaseObject {
     for await (const [handler, data] of this._events.get(name).entries()) {
       event.data = data;
 
-      if (typeof handler === 'string' && typeof handler !== undefined) {
+      if (typeof handler === 'string') {
         // global function
         await invoke(handler, [event]);
       }
@@ -232,5 +240,154 @@ export default class Component extends BaseObject {
 
     // invoke class-level attached handlers
     await Event.trigger(name, event);
+  }
+
+  /**
+   * Returns a list of behaviors that this component should behave as.
+   *
+   * Child classes may override this method to specify the behaviors they want to behave as.
+   *
+   * The return value of this method should be an array of behavior objects or configurations
+   * indexed by behavior names. A behavior configuration can be either a string specifying
+   * the behavior class or an object of the following structure:
+   *
+   * ```js
+   * behaviorName: {
+   *     class: '@path/to/BehaviorClass',
+   *     property1: 'value1',
+   *     property2; 'value2',
+   * }
+   * ```
+   *
+   * Note that a behavior class must extend from {@link Behavior}. Behaviors can be attached using a name or anonymously.
+   * When a name is used as the array key, using this name, the behavior can later be retrieved using {@link getBehavior getBehavior()}
+   * or be detached using {@link detachBehavior detachBehavior()}. Anonymous behaviors can not be retrieved or detached.
+   *
+   * Behaviors declared in this method will be attached to the component automatically (on demand).
+   *
+   * @returns The behavior configurations.
+   */
+  public behaviors(): Record<string, BehaviorInstance> {
+    return {};
+  }
+
+  /**
+   * Returns the named behavior object.
+   * @param name the behavior name
+   * @returns The behavior object, or null if the behavior does not exist
+   */
+  public getBehavior(name: string): BehaviorInstance | null {
+    this.ensureBehaviors();
+    return this._behaviors.has(name)
+      ? this._behaviors.get(name)
+      : null;
+  }
+
+  /**
+   * Returns all behaviors attached to this component.
+   * @returns List of behaviors attached to this component
+   */
+  public getBehaviors(): BehaviorInstance[] {
+    this.ensureBehaviors();
+    return Array.from(this._behaviors.values());
+  }
+
+  /**
+   * Attaches a behavior to this component.
+   * This method will create the behavior object based on the given
+   * configuration. After that, the behavior object will be attached to
+   * this component by calling the {@link Behavior#attach Behavior.attach()} method.
+   * @param name the name of the behavior.
+   * @param behavior the behavior configuration. This can be one of the following:
+   *
+   *  - a {@link Behavior} object
+   *  - a string specifying the behavior class
+   *  - an object configuration array that will be passed to {@link BaseJii#createObject Jii.createObject()} to create the behavior object.
+   *
+   * @return Behavior the behavior object
+   * @see {@link detachBehavior detachBehavior()}
+   */
+  public attachBehavior(name: string, behavior: BehaviorArgs): BehaviorInstance {
+    this.ensureBehaviors();
+    return this.attachBehaviorInternal(name, behavior);
+  }
+
+  /**
+   * Attaches a list of behaviors to the component.
+   *
+   * Each behavior is indexed by its name and should be a {@link Behavior} object,
+   * a string specifying the behavior class, or a configuration array for creating the behavior.
+   * @param behaviors list of behaviors to be attached to the component
+   * @see {@link attachBehavior attachBehavior()}
+   * @see attachBehavior()
+   */
+  public attachBehaviors(behaviors: Record<string, BehaviorArgs>): void {
+    this.ensureBehaviors();
+    for (const [name, behavior] of Object.entries(behaviors)) {
+      this.attachBehaviorInternal(name, behavior);
+    }
+  }
+
+  /**
+   * Detaches a behavior from the component.<br>
+   * The behavior's {@link Behavior#detach Behavior.detach()} method will be invoked.
+   * @param name the behavior's name.
+   * @returns The detached behavior. Null if the behavior does not exist.
+   */
+  public detachBehavior(name: string): BehaviorInstance | null {
+    this.ensureBehaviors();
+
+    if (this._behaviors.has(name)) {
+      const behavior = this._behaviors.get(name);
+      this._behaviors.delete(name);
+      behavior.detach();
+      return behavior;
+    }
+
+    return null;
+  }
+
+  /**
+   * Detaches all behaviors from the component.
+   */
+  public detachBehaviors(): void {
+    this.ensureBehaviors();
+    for (const name of this._behaviors.keys()) {
+      this.detachBehavior(name);
+    }
+  }
+
+  /**
+   * Makes sure that the behaviors declared in {@link behaviors behaviors()} are attached to this component.
+   */
+  public ensureBehaviors(): void {
+    if (!this._behaviors) {
+      this._behaviors.clear();
+
+      for (const [name, behavior] of Object.entries(this.behaviors())) {
+        this.attachBehaviorInternal(name, behavior);
+      }
+    }
+  }
+
+  /**
+   * Attaches a behavior to this component.
+   * @param name - The name of the behavior.
+   * @param behavior - The behavior to be attached
+   * @returns The attached behavior.
+   */
+  public attachBehaviorInternal(name: string, behavior: BehaviorArgs): BehaviorInstance {
+    if (!(behavior instanceof Behavior)) {
+      behavior = Jii.createObject<BehaviorInstance>(behavior);
+    }
+
+    if (this._behaviors.has(name)) {
+      this._behaviors.get(name).detach();
+    }
+
+    behavior.attach(this);
+    this._behaviors.set(name, behavior);
+
+    return behavior;
   }
 }
