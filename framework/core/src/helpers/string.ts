87aba142b82215f6cab019a5e13bc65e35bb9dcc
Junaid Atari <mj.atari@gmail.com>
2024-01-14 08:38:41 +0500

    add utility functions

--- framework/core/src/helpers/string.ts
+++ framework/core/src/helpers/string.ts
@@ -141,3 +141,125 @@ export const mask = (string: string, start: number, length: number, mask: string
   return masked;
 };
 
+/**
+ * Strip whitespace (or other characters) from a string
+ * @param str - The input string.
+ * @param dir - Direction to replace from let or right or both
+ * @param [characters] - You can also specify the characters you want to strip, by means
+ * of the characters' parameter. Simply list all characters that you want to be stripped.
+ * With .. you can specify a range of characters.
+ * @return This function returns a string with whitespace stripped from a string.
+ * @see https://locutus.io/php/strings/ltrim/
+ * @private
+ */
+const _lr_trim = (str: string, dir: 'left' | 'right' | 'both', characters: string = ' \\\\s\u00A0'): string => {
+  characters = !characters.trim()
+    ? ' \\s\u00A0'
+    : (characters + '').replace(/([[\]().?/*{}+$^:])/g, '$1');
+
+  let rgx = `[${characters}]+`;
+  dir === 'left' && (rgx = '^' + rgx);
+  dir === 'right' && (rgx = rgx + '$');
+
+  const re = new RegExp(rgx, 'g');
+  return (str + '').replace(re, '');
+};
+
+/**
+ * Strip whitespace (or other characters) from the beginning of a string
+ * @param str - The input string.
+ * @param [characters] - You can also specify the characters you want to strip, by means
+ * of the characters' parameter. Simply list all characters that you want to be stripped.
+ * With .. you can specify a range of characters.
+ * @return This function returns a string with whitespace stripped from the beginning of string.
+ * @see https://locutus.io/php/strings/ltrim/
+ */
+export const ltrim = (str: string, characters: string = ' \\\\s\u00A0'): string => {
+  return _lr_trim(str, 'left', characters);
+};
+
+/**
+ * Strip whitespace (or other characters) from the end of a string
+ * @param str - The input string.
+ * @param [characters] - You can also specify the characters you want to strip, by means
+ * of the characters' parameter. Simply list all characters that you want to be stripped.
+ * With .. you can specify a range of characters.
+ * @return This function returns a string with whitespace stripped from the end of string.
+ */
+export const rtrim = (str: string, characters: string = ' \\\\s\u00A0'): string => {
+  return _lr_trim(str, 'right', characters);
+};
+
+/**
+ * Uppercase the first character of each word in a string
+ * @param str - The input string.
+ * @returns Returns the modified string.
+ * @see https://locutus.io/php/strings/ucwords/
+ */
+export const ucwords = (str: string): string => {
+  return toString(str).replace(/^(.)|\s+(.)/g, $1 => $1.toUpperCase());
+};
+
+/**
+ * Find the position of the first occurrence of a substring in a string
+ * @param haystack - The string to search in.
+ * @param needle - The string to search for.
+ * @param offset - If specified, search will start this number of characters counted from the
+ * beginning of the string. If the offset is negative, the search will start this number of
+ * characters counted from the end of the string.
+ * @returns Returns the position of where the needle exists relative to the beginning of the
+ * haystack string (independent of offset). Also note that string positions start at 0, and not 1.
+ *
+ * @example
+ * strpos('Kevin van Zonneveld', 'e', 5) // 14
+ */
+export const strpos = (haystack: string, needle: string, offset: number = 0): number | false => {
+  const i = toString(haystack).indexOf(needle, (offset || 0));
+  return i === -1 ? false : i;
+};
+
+/**
+ * Return part of a string
+ * @param string - The input string.
+ * @param offset - If offset is non-negative, the returned string will start at the offset'th position in string, counting from zero. For instance, in the string 'abcdef', the character at position 0 is 'a', the character at position 2 is 'c', and so forth.
+ * If offset is negative, the returned string will start at the offset'th character from the end of string.
+ * If string is less than offset characters long, an empty string will be returned.
+ *
+ * @param length - If length is given and is positive, the string returned will contain at most length characters beginning from offset (depending on the length of string).
+ * If length is given and is negative, then that many characters will be omitted from the end of string (after the start position has been calculated when a offset is negative). If offset denotes the position of this truncation or beyond, an empty string will be returned.
+ * If length is given and is 0, an empty string will be returned.
+ * If length is omitted or null, the substring starting from offset until the end of the string will be returned.
+ * @returns Returns the extracted part of string, or an empty string.
+ *
+ * @example
+ * substr("abcdef", 0, -1);  // returns "abcde"
+ * substr("abcdef", 2, -1);  // returns "cde"
+ * substr("abcdef", 4, -4);  // returns ""; prior to PHP 8.0.0, false was returned
+ * substr("abcdef", -3, -1); // returns "de"
+ *
+ * @see https://locutus.io/php/strings/substr/
+ */
+export const substr = (string: string, offset: number, length: number = undefined): string | false => {
+  string = toString(string);
+
+  const inputLength = string.length;
+  let end = inputLength;
+
+  if (offset < 0) {
+    offset += end;
+  }
+
+  if (length) {
+    if (length < 0) {
+      end = length + end;
+    } else {
+      end = length + offset;
+    }
+  }
+
+  if (offset > inputLength || offset < 0 || offset > end) {
+    return false;
+  }
+
+  return string.slice(offset, end);
+};
