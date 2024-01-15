1a43f6e2a0e815b86246d5fef18f5fd2eb1c09f1
Junaid Atari <mj.atari@gmail.com>
2024-01-15 12:56:53 +0500

    update reference

--- packages/main/src/index.ts
+++ packages/main/src/index.ts
@@ -1,5 +1,13 @@
+/**
+ * @link https://www.github.com/jiifw
+ * @copyright Copyright (c) 2023-2024 Jii framework
+ * @license https://www.github.com/jiifw/jii/blob/main/LICENSE
+ * @author Junaid Atari <mj.atari@gmail.com>
+ * @since 0.0.1
+ */
+
 import {loadFile} from '@jii/core/dist/env';
-import Application from '@jii/server/dist/web/Application';
+import Application from '@jii/server/dist/classes/Application';
 import appConfig from './config/app';
 
 loadFile('../.env');
