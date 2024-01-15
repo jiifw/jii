f9601014ec44d5ae0b0b74e9b54cef6d9686fb53
Junaid Atari <mj.atari@gmail.com>
2024-01-15 10:45:38 +0500

    add/set basic properties

--- framework/core/src/classes/Application.ts
+++ framework/core/src/classes/Application.ts
@@ -6,19 +6,24 @@
  * @since 0.0.1
  */
 
+import objPath from 'object-path';
+
 // classes
 import Module from './Module';
 
 // utils
 import Jii from '../Jii';
+import {toString} from '../helpers/string';
+import {isPlainObject} from '../helpers/object';
 import {APP_CONFIG, CONTAINER_APP_KEY} from '../utils/symbols';
 
 // scripts
 import configValidator from '../scripts/config-validator';
 
 // types
-import {ApplicationConfig} from '../typings/app-config';
 import {Props} from './BaseObject';
+import InvalidConfigError from './InvalidConfigError';
+import {ApplicationConfig, ComponentsDefinition} from '../typings/app-config';
 
 /**
  * Application is the base class for all application classes.
@@ -47,6 +52,31 @@ export default abstract class Application<
   public state: number;
 
   /**
+   * The application name.
+   */
+  public name: string = 'My Application';
+
+  /**
+   * The time zone used by this application.
+   */
+  public timeZone: string = 'UTC';
+
+  /**
+   * The language that is meant to be used for end users. It is recommended that you
+   * use [IETF language tags](http://en.wikipedia.org/wiki/IETF_language_tag). For example, `en` stands
+   * for English, while `en-US` stands for English (United States).
+   * @see sourceLanguage
+   */
+
+  public language: string = 'en-US';
+  /**
+   * The language that the application is written in. This mainly refers to
+   * the language that the messages and view files are written in.
+   * @see language
+   */
+  public sourceLanguage: string = 'en-US';
+
+  /**
    * Application constructor
    * @param config - Application configuration
    * @param [props] - Component properties
@@ -60,6 +90,29 @@ export default abstract class Application<
   }
 
   /**
+   * Pre-initializes the application configuration<br>
+   * This method is called at the beginning of the application constructor.<br>
+   * It initializes several important application properties.<br>
+   * If you override this method, please make sure you call the parent implementation.<br>
+   * @param config - The application configuration
+   */
+  public preInitConfig(config: ApplicationConfig): void {
+    if (!objPath.has(config, 'components') || !isPlainObject(config.components)) {
+      config.components = {};
+    }
+
+    // merge core components with custom components
+    for (const [id, component] of Object.entries(this.coreComponents())) {
+      if (!objPath.has(config, ['components', id])) {
+        config['components'][id] = component;
+      } else if (isPlainObject(config.components[id])
+        && !objPath.has(config, ['components', id, 'class'])) {
+        objPath.set(config, ['components', id, 'class'], component['class']);
+      }
+    }
+  }
+
+  /**
    * Pre-initializes the application.<br>
    * This method is called at the beginning of the application constructor.<br>
    * It initializes several important application properties.<br>
@@ -67,20 +120,53 @@ export default abstract class Application<
    * @param config - The application configuration
    * @throws InvalidConfigError if either {@link id} or {@link basePath} configuration is missing.
    */
-  public preInit(config: T) {
+  public preInit(config: T): void {
+    this.preInitConfig(config);
+
     // validates/verify configuration
     configValidator(<ApplicationConfig>config);
 
+    if (!toString(config?.id, true)) {
+      throw new InvalidConfigError('The "id" configuration for the Application is required.');
+    }
+
+    // memorize the configuration for future reference and usage
+    Jii.container.memoSync(APP_CONFIG, config, {freeze: true});
+
     this.id = config.id;
-    this.params = config.params;
+    delete config.id;
+
+    if (toString(config?.name, true)) {
+      this.name = config.name;
+      delete config.name;
+    }
+
+    if (toString(config?.language, true)) {
+      this.language = config.language;
+      delete config.language;
+    }
+
+    if (toString(config?.sourceLanguage, true)) {
+      this.sourceLanguage = config.sourceLanguage;
+      delete config.sourceLanguage;
+    }
+
+    if (toString(config?.timeZone, true)) {
+      this.timeZone = config.timeZone;
+      delete config.timeZone;
+    }
+
+    if (isPlainObject(config?.params)) {
+      this.params = config.params;
+      delete config.params;
+    }
+
     this.setBasePath(config.basePath);
 
     // register aliases
     this.setAliases(config.aliases);
 
     this.setComponents(config?.components);
-
-    Jii.container.memoSync(APP_CONFIG, config, {freeze: true});
   }
 
   /**
@@ -93,5 +179,13 @@ export default abstract class Application<
    */
   init() {
     this.state = Application.STATE_INIT;
+  }
+
+  /**
+   * Returns the configuration of core application components.
+   * @see set()
+   */
+  public coreComponents(): ComponentsDefinition {
+    return {};
   }
 }
