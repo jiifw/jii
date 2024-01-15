220388857a99ed476511250418b2cea7e4320ca2
Junaid Atari <mj.atari@gmail.com>
2024-01-15 12:46:29 +0500

    omit middleware code

--- packages/main/src/bootstrap/bootstrap.ts
+++ packages/main/src/bootstrap/bootstrap.ts
@@ -1,28 +1,19 @@
-// utils
-import {applyMiddleware} from '@jii/server/dist/middleware';
-import Jii from '@jii/core/dist/Jii';
+/**
+ * @link https://www.github.com/jiifw
+ * @copyright Copyright (c) 2023-2024 Jii framework
+ * @license https://www.github.com/jiifw/jii/blob/main/LICENSE
+ * @author Junaid Atari <mj.atari@gmail.com>
+ * @since 0.0.1
+ */
 
 /**
  * Server bootstrapper
  */
 export default async (): Promise<void> => {
-  Jii.container.memoSync('testing', () => new Map(), {freeze: true});
+  // Jii.container.memoSync('testing', () => new Map(), {freeze: true});
   //Jii.container.flush('testing')
   //Jii.container.setConfig('testing', {testing: 'xx'});
   //console.log('Jii', Jii.container);
-
-  await applyMiddleware([
-    /** Register plugins/middleware here */
-    { path: '@jii/cors/dist', type: 'plugin' },
-    //{ path: '@framework/plugins/swagger', type: 'plugin' },
-    //{ path: '@framework/plugins/session', type: 'plugin' },
-    //{ path: '@framework/plugins/access-token', type: 'plugin' },
-    //{ path: '@framework/plugins/jwt', type: 'plugin' },
-    //{ path: '@framework/plugins/i18n', type: 'plugin' },
-    //{ path: '@framework/db/sequelize', type: 'plugin' },
-  ]);
-
-  //Jii.middleware.setAttr('cors', 'test', 'testing')
 
   //console.log('Jii.middleware:', Jii.middleware);
   //console.log('metadata():', Jii.middleware.metadata('cors'));
