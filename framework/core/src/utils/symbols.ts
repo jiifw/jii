88b6988d68e2fc938a552067af6f00a4e7dc7473
Junaid Atari <mj.atari@gmail.com>
2024-01-16 21:47:06 +0500

    change symbol name

--- framework/core/src/utils/symbols.ts
+++ framework/core/src/utils/symbols.ts
@@ -1,5 +1,5 @@
 export const INTERNAL_METADATA = Symbol.for('__internal-metadata');
-export const CONTAINER_MIDDLEWARE_KEY = Symbol.for('__middleware');
+export const CONTAINER_PLUGINS_KEY = Symbol.for('__plugins');
 export const CONTAINER_APP_KEY = Symbol.for('__app');
 export const INTERNAL_CLI_DIRS = Symbol.for('__cli.dirs');
 export const APP_CONFIG = Symbol.for('__app_config');
