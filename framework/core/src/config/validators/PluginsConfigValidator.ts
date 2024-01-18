53911589abd0afde91b5e5fdbd840db1a00d2585
Junaid Atari <mj.atari@gmail.com>
2024-01-18 11:57:08 +0500

    create plugin validator

---
+++ framework/core/src/config/validators/PluginsConfigValidator.ts
@@ -0,0 +1,80 @@
+/**
+ * @link https://www.github.com/jiifw
+ * @copyright Copyright (c) 2023-2024 Jii framework
+ * @license https://www.github.com/jiifw/jii/blob/main/LICENSE
+ * @author Junaid Atari <mj.atari@gmail.com>
+ * @since 0.0.1
+ */
+
+import merge from 'deepmerge';
+import {join, normalize} from 'node:path';
+
+// classes
+import ConfigValidator from '../../classes/ConfigValidator';
+import InvalidConfigError from '../../classes/InvalidConfigError';
+import {Schema, PropertyPath} from '../../classes/ConfigValidator';
+
+// utils
+import Jii from '../../Jii';
+import {isPath} from '../../helpers/path';
+import {readSchemaFile} from '../../helpers/file';
+
+// types
+import {PluginsDefinition} from '../../typings/plugin';
+
+/**
+ * Validate and register plugins into to the application
+ */
+export default class PluginsConfigValidator extends ConfigValidator {
+  /**
+   * @inheritDoc
+   */
+  public propertyName(): PropertyPath | PropertyPath[] {
+    return 'plugins';
+  }
+
+  /**
+   * @inheritDoc
+   */
+  async getSchema(): Promise<Schema | null> {
+    return readSchemaFile('@jiiRoot/schemas/plugin');
+  }
+
+  /**
+   * @inheritDoc
+   */
+  async validate(): Promise<void> {
+    const definitions = this.getConfig() as unknown as PluginsDefinition;
+    const plugins = Object.entries(definitions);
+
+    if (!plugins.length) {
+      return;
+    }
+
+    for (const [id, def] of plugins) {
+      const config = merge({file: 'index'} as unknown as PluginsDefinition, def);
+      const basePath = normalize(Jii.getAlias(config.path));
+
+      if (!isPath(basePath, 'dir')) {
+        throw new InvalidConfigError(`Plugin '${id}' path '${config.path}' must be a valid directory`);
+      }
+
+      const filePath = require.resolve(join(basePath, config.file));
+
+      if (!isPath(filePath, 'file')) {
+        throw new InvalidConfigError(`Plugin '${id}' file '${config.file}' must be a valid file`);
+      }
+    }
+  }
+
+  /**
+   * @inheritDoc
+   */
+  async apply(): Promise<void> {
+    const plugins = this.getConfig() as unknown as PluginsDefinition;
+
+    if (Object.keys(plugins).length) {
+      Jii.plugins.setPlugins(plugins, false);
+    }
+  }
+}
