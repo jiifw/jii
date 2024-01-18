5b03aa500fdde8a9d836b97b56aa5f80cfd014ed
Junaid Atari <mj.atari@gmail.com>
2024-01-18 11:12:35 +0500

    add utility functions

--- framework/core/src/helpers/array.ts
+++ framework/core/src/helpers/array.ts
@@ -1,4 +1,12 @@
 /**
+ * @link https://www.github.com/jiifw
+ * @copyright Copyright (c) 2023-2024 Jii framework
+ * @license https://www.github.com/jiifw/jii/blob/main/LICENSE
+ * @author Junaid Atari <mj.atari@gmail.com>
+ * @since 0.0.1
+ */
+
+/**
  * Wraps an array or any value into an array
  * @param [arg] - Value to wrap
  * @returns Array of the value
@@ -11,4 +19,29 @@
  */
 export const wrap = <T = any>(arg: T): T[] => {
   return Array.isArray(arg) ? arg : [arg];
+};
+
+/**
+ * Checks that list and haystack list items are valid and match
+ * @param list - Input list
+ * @param haystack - List to check and match with
+ * @returns Whatever the list is matched or not
+ */
+export const isSame = <T = any>(list: T[], haystack: T[]): boolean => {
+  if (!Array.isArray(list) || !list.length) return false;
+  if (!Array.isArray(haystack) || !haystack.length) return false;
+  return list.filter(val => haystack.includes(val)).length === list.length;
+};
+
+/**
+ * Checks any list item or items include in haystack list
+ * @param list - Input list
+ * @param haystack - List to check and match in
+ * @returns Whatever the items(s) present or not
+ */
+export const contains = <T = any>(list: T[], haystack: T[]): boolean => {
+  if (!Array.isArray(list) || !list.length) return false;
+  if (!Array.isArray(haystack) || !haystack.length) return false;
+
+  return !!list.filter(val => haystack.includes(val)).length;
 };
