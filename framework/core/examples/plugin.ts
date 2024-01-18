2c97918ef89fa4ad325b332e9c79f8354ab11c5b
Junaid Atari <mj.atari@gmail.com>
2024-01-18 13:19:27 +0500

    add 'plugin' example

---
+++ framework/core/examples/plugin.ts
@@ -0,0 +1,64 @@
+/**
+ * @link https://www.github.com/jiifw
+ * @copyright Copyright (c) 2023-2024 Jii framework
+ * @license https://www.github.com/jiifw/jii/blob/main/LICENSE
+ * @author Junaid Atari <mj.atari@gmail.com>
+ * @since 0.0.1
+ */
+
+// classes
+import Application from '../src/classes/Application';
+
+// types
+import {ApplicationConfig} from '../src/typings/app-config';
+
+class MyApplication extends Application {
+  /**
+   * @inheritDoc
+   */
+  protected _platform = 'web';
+}
+
+const config: ApplicationConfig = {
+  id: 'xyz-name',
+  basePath: __dirname,
+  plugins: {
+    myPlugin: {
+      path: '@jiiRoot/plugins/test-plugin',
+      config: {foo: 'bar'} as any,
+      components: {
+        serverXys: {
+          class: '@jiiRoot/classes/Component',
+          enableLogging: false,
+        },
+      },
+      [`on ${Application.EVENT_BEFORE_FINALIZE_CONFIG}`]: () => {
+        // implement logic here
+      },
+    },
+  },
+};
+
+(async () => {
+  const app = new MyApplication(config);
+  await app.run();
+})();
+
+/*
+// file: plugins/test-plugin/index.ts
+
+import Plugin from '../../classes/Plugin';
+import {EventHandler} from '../../classes/Event';
+
+export default class TestPlugin extends Plugin {
+  events(): Record<string, EventHandler> {
+    return {
+      [Plugin.EVENT_BEFORE_APP_RUN]: 'beforeAppRun',
+    };
+  }
+
+  public async beforeAppRun(evt) {
+    //console.log(`Event '${Plugin.EVENT_BEFORE_APP_RUN}' triggered from`, this.constructor.name);
+  }
+}
+*/
