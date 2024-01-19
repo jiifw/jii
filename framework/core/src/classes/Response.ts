0ff47efaf594334464ed480efbf395bb0fec1713
Junaid Atari <mj.atari@gmail.com>
2024-01-19 22:12:06 +0500

    remove methods

--- framework/core/src/classes/Response.ts
+++ framework/core/src/classes/Response.ts
@@ -10,33 +10,12 @@
 import Component from './Component';
 
 /**
- * The web Response class represents an HTTP response.
- *
- * Response is configured as an application component in {@link Application} by default.
- * You can access that instance via `Jii.app().get<Response>('response')`.
- *
+ * Response represents the response of an {@link Application} to a {@link Request}.
  */
 export default class Response extends Component {
   /**
-   * Returns the header collection.
-   * The header collection contains the currently registered HTTP headers.
-   * @returns The header collection
+   * Sends the response to client.
    */
-  public getHeaders(): Record<string, string> {
-    return {};
-  }
-
-  /**
-   * @return Whether this response indicates the currently requested resource is not found
-   */
-  public getIsNotFound(): boolean {
-    return false;
-  }
-
-  /**
-   * @return Whether this response is empty
-   */
-  public getIsEmpty(): boolean {
-    return false;
+  public async send() {
   }
 }
