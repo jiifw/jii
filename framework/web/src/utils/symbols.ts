bb217675dbac07b8999eff67f7e4943fb5454786
Junaid Atari <mj.atari@gmail.com>
2024-01-19 22:57:09 +0500

    create symbols file

---
+++ framework/web/src/utils/symbols.ts
@@ -0,0 +1,11 @@
+/**
+ * @link https://www.github.com/jiifw
+ * @copyright Copyright (c) 2023-2024 Jii framework
+ * @license https://www.github.com/jiifw/jii/blob/main/LICENSE
+ * @author Junaid Atari <mj.atari@gmail.com>
+ * @since 0.0.1
+ */
+
+export const SERVER_REQUEST = Symbol.for('__server_request');
+
+export const SERVER_REPLY = Symbol.for('__server_reply');
