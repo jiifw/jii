bf3565011bde585988a5baa38cf5accee8902ee3
Junaid Atari <mj.atari@gmail.com>
2024-01-22 17:15:49 +0500

    add components defs typing

--- framework/web/src/typings/app-config.ts
+++ framework/web/src/typings/app-config.ts
@@ -8,10 +8,31 @@
 
 // types
 import {ServerComponentDefinition} from './classes/Server';
+import {RequestComponentDefinition} from './classes/Request';
+import {ResponseComponentDefinition} from './classes/Response';
+import {AccessTokenComponentDefinition} from './classes/AccessToken';
 import {ApplicationConfig as IAppConfig, ComponentsDefinition} from '@jii/core/dist/typings/app-config';
 
 export interface ApplicationConfig extends IAppConfig {
   components?: ComponentsDefinition & {
+    /**
+     * Server component configuration.
+     */
     server?: ServerComponentDefinition;
+
+    /**
+     * Request component configuration.
+     */
+    request?: RequestComponentDefinition;
+
+    /**
+     * Response component configuration.
+     */
+    response?: ResponseComponentDefinition;
+
+    /**
+     * Access token component configuration.
+     */
+    accessToken?: AccessTokenComponentDefinition;
   };
 }
