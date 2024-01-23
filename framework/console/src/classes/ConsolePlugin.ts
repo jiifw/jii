0190dc4da6fb5cd1b2eb4252c271da31da982000
Junaid Atari <mj.atari@gmail.com>
2024-01-23 14:55:01 +0500

    create base plugin class

---
+++ framework/console/src/classes/ConsolePlugin.ts
@@ -0,0 +1,15 @@
+/**
+ * @link https://www.github.com/jiifw
+ * @copyright Copyright (c) 2023-2024 Jii framework
+ * @license https://www.github.com/jiifw/jii/blob/main/LICENSE
+ * @author Junaid Atari <mj.atari@gmail.com>
+ * @since 0.0.1
+ */
+
+import Plugin from '@jii/core/dist/classes/Plugin';
+
+/**
+ * A base plugin class to create console application specific plugins
+ */
+export default class ConsolePlugin extends Plugin {
+}
