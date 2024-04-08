4b5e8b801e01bf1e965dfa6356ce1756aae6b2ab
Junaid Atari <mj.atari@gmail.com>
2024-04-08 12:53:55 +0500

    add types definition file

---
+++ plugins/jii-wizard/src/types.ts
@@ -0,0 +1,17 @@
+/**
+ * Friendly CORS Plugin
+ * @link https://www.github.com/jiifw/jii/plugins/jii-cors
+ * @copyright Copyright (c) 2023-2024 Jii framework
+ * @license https://www.github.com/jiifw/jii/blob/main/LICENSE
+ * @author Junaid Atari <mj.atari@gmail.com>
+ * @version 0.0.1
+ */
+
+// types
+import {PluginDefinition} from '@jii/core/dist/typings/plugin';
+
+/**
+ * Wizard plugin definition
+ */
+export interface WizardPluginDefinition extends PluginDefinition {
+}
